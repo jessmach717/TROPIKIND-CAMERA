5c8b12065d1f541ed9eacbce39fe290f420e36ec
jessmach717 <jdaypuyart@gmail.com>
2026-09-22 07:55:40 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tropikind-camera-v3';
+const CACHE = 'tropikind-camera-v4';
 const CORE = ['./', './index.html', './manifest.webmanifest',
   './icon-192.png', './icon-512.png', './maskable-512.png'];
 
